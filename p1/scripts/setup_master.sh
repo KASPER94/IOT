89cec866f54ed9f157fa7fe5dddc254526d1fb28
simon <simon@zmiot.myguest.virtualbox.org>
2025-03-06 11:39:36 +0100

    Finally fix p1

--- p1/scripts/setup_master.sh
+++ p1/scripts/setup_master.sh
@@ -10,6 +10,7 @@ NODE_IP=$(ip address | grep eth1 | grep 'inet ' | awk '{print $2}' | cut -d '/' 
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--node-ip=${NODE_IP}" sh -
 CONFIG_FILE="/etc/rancher/k3s/k3s.yaml"
+TOKEN_FILE="/var/lib/rancher/k3s/server/node-token"
 
 for i in {1..30}; do
     if [ -f "$CONFIG_FILE" ]; then
@@ -21,6 +22,15 @@ done
 
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 sudo chown vagrant:vagrant /etc/rancher/k3s/k3s.yaml
+
+for i in {1..30}; do
+  if [ -f "$TOKEN_FILE" ]; then
+    echo "found Config File"
+    break
+  fi
+  sleep 2
+done
+cat "$TOKEN_FILE" > /vagrant/token
 
 echo "K3s (controller) installé avec succès sur Server"
 
--- p1/scripts/setup_worker.sh
+++ p1/scripts/setup_worker.sh
@@ -6,8 +6,9 @@ sudo apt install curl -y
 sudo swapoff -a
 
 SERVER_IP="192.168.56.110"
-TOKEN=$(ssh -o StrictHostKeyChecking=no vagrant@$SERVER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
+# TOKEN=$(ssh -o StrictHostKeyChecking=no vagrant@$SERVER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
+TOKEN=$(cat /vagrant/token)
 
-curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$TOKEN" sh -
+curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN="$TOKEN" INSTALL_K3S_EXEC="--node-ip=${SERVER_IP}" sh -
 
 echo "K3s (agent) installé avec succès sur ServerWorker"
